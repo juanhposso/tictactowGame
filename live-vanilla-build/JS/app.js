25675f050c161bebe8ddaf2c24cd280454f164e3
juanhposso <juan.hernandez0793@gmail.com>
2023-08-27 12:27:48 -0500

    tracking the current state of the game adding the logic to change between players

--- live-vanilla-build/JS/app.js
+++ live-vanilla-build/JS/app.js
@@ -9,7 +9,7 @@ const App = {
 	},
 
 	state: {
-		currentPlayer: 1,
+		moves: [],
 	},
 
 	init() {
@@ -31,20 +31,55 @@ const App = {
 
 		App.$.squares.forEach((square) => {
 			square.addEventListener("click", (event) => {
-				console.log(`Square with id ${event.target.id} was clicked`);
+				//* Check if the square is already check with "X" or "O"
+				const hasMove = (squareID) => {
+					const existingMove = App.state.moves.find(
+						(move) => move.squareID === squareID
+					);
 
-				const currentPlayer = App.state.currentPlayer; // default 1
+					return existingMove !== undefined;
+				};
 
+				if (hasMove(+square.id)) {
+					console.log("estoy aqui");
+					return;
+				}
+
+				//* Set up the current player
+				const lastMove = App.state.moves.at(-1);
+				//console.log(lastMove);
+				const getOppositePlayer = (playerID) => (playerID === 1 ? 2 : 1);
+				const currentPlayer =
+					App.state.moves.length === 0
+						? 1
+						: getOppositePlayer(lastMove.playerID);
+
+				//* Create the nodeElement
 				const icon = document.createElement("i");
 
+				//* Check what the current player is to place in the square "X" or "O"
 				if (currentPlayer === 1) {
 					icon.classList.add("fa-solid", "fa-x", "yellow");
 				} else {
 					icon.classList.add("fa-solid", "fa-o", "turquoise");
 				}
 
-				event.target.replaceChildren(icon);
-				App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+				//* ADD in the square the "X" or "O"
+				square.replaceChildren(icon);
+
+				App.state.moves.push({ squareID: +square.id, playerID: currentPlayer });
+
+				//* Detect if there is a winner or tie LOGIC
+				const winningPatterns = [
+					[1, 2, 3],
+					[1, 5, 9],
+					[1, 4, 7],
+					[2, 5, 8],
+					[3, 5, 7],
+					[3, 6, 9],
+					[4, 5, 6],
+					[7, 8, 9],
+				];
 			});
 		});
 	},
